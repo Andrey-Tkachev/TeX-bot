b8c101735e30788cde30620d1065efeff572c039
Tkachev Andrey <tka4.andr@yandex.ru>
2016-12-26 21:56:08 +0300

    add config and log libs, comments

--- bot.js
+++ bot.js
@@ -1,43 +1,57 @@
+// import bot framework
 var TelegramBot = require('node-telegram-bot-api');
-var fs          = require('fs');
-var path        = require('path');
 
+// import libs for work with system
+var fs          = require('fs'); // file-system
+var path        = require('path'); // for generating pathnames
+var log         = require('./libs/log')(module);
+var config      = require('./libs/config');
 
-var token = '321732536:AAFOOSuPAA5eB6yKk9uvNByUpwFIuhJHvlQ';
+const token = config.get('token');
 
 var bot = new TelegramBot(token, { polling: true });
 
-const images_dir = 'images'
-const spawn = require('child_process').spawn;
+const images_dir = config.get('images-dir');
+const images_ext = config.get('images-ext');
+const images_res = config.get('resolution');
+
+const messages = config.get('bot-messages');
+
+const spawn = require('child_process').spawn; // for running bash scripts
 
 bot.onText(/\/tex (.+)/, function (msg, match) {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
+
+  // name of res file without extension
   var filename  = 'test';
-  var extension = '.png'
 
   var data = match[1];
+  // create file with message as content
   fs.writeFile(filename, data, function (err) {
       if (err) return console.log(err);
 
       var chatId = msg.chat.id;
-      var path2res = path.join(images_dir, filename + extension);
-      const tex2im = spawn('tex2im', ['-r', '300x300',
+      var path2res = path.join(images_dir, filename + images_ext);
+
+      const tex2im = spawn('tex2im', ['-r', images_res,
                                       '-o', path2res, filename]);
       tex2im.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
+        log.error(`stdout: ${data}`);
       });
 
       tex2im.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
+        log.error(`stderr: ${data}`);
       });
 
       tex2im.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
+        log.info(`child process exited with code ${code}`);
         fs.exists(path2res, function (exists) {
               if (exists)
-                bot.sendPhoto(chatId, path2res, {caption: "It's your TeX!"});
+                bot.sendPhoto(chatId, path2res, {
+                  caption: messages['send-photo']
+                });
         });
       });
   });
@@ -46,6 +60,5 @@ bot.onText(/\/tex (.+)/, function (msg, match) {
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  // send a message to the chat acknowledging receipt of their message
+  // Do something
 });
