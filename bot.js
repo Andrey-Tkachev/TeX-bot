73768101d1f468e15d5561e2689dcb48c3d31b4f
Tkachev Andrey <tka4.andr@yandex.ru>
2017-01-03 16:34:43 +0300

    add inline funtional[need to test on real server], cute config_sample

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ var fs          = require('fs'); // file-system
 var path        = require('path'); // for generating pathnames
 var log         = require('./libs/log')(module);
 var config      = require('./libs/config');
+var static      = require('node-static');
 
 const token = config.get('token');
 
@@ -15,45 +16,67 @@ const tex_dir = config.get('tex-dir');
 const images_dir = config.get('images-dir');
 const images_ext = config.get('images-ext');
 const images_res = config.get('resolution');
-
+const server_ip = config.get('ip');
+const server_port = config.get('static-port');
 const messages = config.get('bot-messages');
+const tex2im_ex = config.get('tex2im');
+// Server to images for inline mode
+var static_files = new static.Server(`./${images_dir}`);
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        static_files.serve(request, response);
+    }).resume();
+}).listen(server_port);
 
-const spawn = require('child_process').spawn; // for running bash scripts
+var spawn = require('child_process').spawn; // for running bash scripts
 
-bot.onText(/\/tex (.+)/, function (msg, match) {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+function inlineQueryResultPhotoFactory(id, photo_url) {
+  return {
+    type: 'photo',
+    id: id,
+    photo_url: photo_url
+  }
+}
 
-  // name of res file without extension
-  var filename  = msg.message_id;
+function tex2png(data, filename, callback) {
   var path2preprocess = path.join(tex_dir, filename + '.tex');
-  var data = match[1];
-  // create file with message as content
   fs.writeFile(path2preprocess, data, function (err) {
       if (err) return console.log(err);
-
-      var chatId = msg.chat.id;
       var path2res = path.join(images_dir, filename + '.' + images_ext);
-      const tex2im = spawn('tex2im', ['-r', images_res,
-                                      '-o', path2res, path2preprocess]);
-      tex2im.stdout.on('data', (data) => {
-        log.error(`stdout: ${data}`);
-      });
+      var tex2im = spawn(tex2im_ex, ['-r', images_res,
+                                      '-o', path2res,
+                                      '-z', path2preprocess], {stdio:'inherit'});
 
-      tex2im.stderr.on('data', (data) => {
-        log.error(`stderr: ${data}`);
+      tex2im.on('error', function(err){
+        log.error('parentError:', err);
       });
 
       tex2im.on('close', (code) => {
         log.info(`child process exited with code ${code}`);
         fs.exists(path2res, function (exists) {
-              if (exists)
-                bot.sendPhoto(chatId, path2res, {
-                  caption: messages['send-photo']
-                });
+              if (exists) {
+                callback(path2res);
+              }
         });
       });
+  });
+}
+
+
+bot.onText(/\/tex (.+)/, function (msg, match) {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  // name of res file without extension
+  log.info('simple query');
+  var filename  = msg.message_id;
+  var data = match[1];
+  var chatId = msg.chat.id;
+  // create file with message as content
+  tex2png(data, filename, function (path2res) {
+    bot.sendPhoto(chatId, path2res, {
+        caption: messages['send-photo']
+    });
   });
 });
 
@@ -61,4 +84,17 @@ bot.onText(/\/tex (.+)/, function (msg, match) {
 // messages.
 bot.on('message', function (msg) {
   // Do something
+});
+
+bot.on('inline_query', function(query) {
+  log.info('inline query');
+  var queryId = query.id;
+  var filename = query.id + '_inline';
+  var data = query.query;
+  tex2png(data, filename, function (path2res) {
+    var resUrl = `${server_ip}:${server_port}/${filename}.${extension}`;
+    var result = inlineQueryResultPhotoFactory('1', resUrl); // 1 becouse we have only one result
+    bot.answerInlineQuery(queryId, [result]);
+  });
+
 });
