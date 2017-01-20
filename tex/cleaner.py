e210dc24eb144ea7da400c9dc2d2482e04dac160
Movses Elbakian <mosik454@mail.ru>
2017-01-21 00:53:11 +0300

    cleaner.py was modified for working at the server

--- tex/cleaner.py
+++ tex/cleaner.py
@@ -1,10 +1,11 @@
+#!usr/bin/python3
 import os, time
 
-del_dirs = [os.path.curdir]
+del_dirs = ['~/Tex_bot/tex', '~/Tex_bot/images']
 del_time = 60
 
 for path in del_dirs:
     file_list = os.listdir(path)
     for file in file_list:
-        if (time.time() - os.path.getmtime(file)) >= del_time and file != 'cleaner.py':
-            os.remove(file)
+        if (time.time() - os.path.getmtime(path + '/' + file)) >= del_time and file != 'cleaner.py':
+            os.remove(path + '/' + file)
